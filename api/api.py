00b09e13dc0b1685f5673da376fd18ae9fb79132
Dona Ganzorig <ganzorig0518@uchicago.edu>
2020-03-08 03:18:46 -0500

    Got endpoints with MongoDb connection working

--- api/api.py
+++ api/api.py
@@ -1,8 +1,38 @@
-import time
-from flask import Flask
+import os
+from flask import Flask, jsonify
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+mongo = PyMongo(app)
 
-@app.route('/time')
-def get_current_time():
-	return { 'time': time.time() }
+# example date retrieve: event['_id'].generation_time
+
+@app.route('/event', methods=['GET'])
+def get_events():
+	events_query = mongo.db.events.find()
+	return events_to_json(events_query)
+
+@app.route('/event/<user_id>', methods=['GET'])
+def get_user_events(user_id):
+	events_query = mongo.db.events.find({ 'auth0_uid': user_id })
+	return events_to_json(events_query)
+
+
+## HELPERS ##
+
+def events_to_json(mongo_cursor):
+	event_jsons = []
+	for event in mongo_cursor:
+		event_json = {
+			'creation_date': event['_id'].generation_time,
+			'place_id': event['place_id'],
+			'auth0_uid': event['auth0_uid'],
+			'coords': {
+				'lat': event['coords']['lat'],
+				'lng': event['coords']['lng']
+			}
+		}
+		event_jsons.append(event_json)
+	return jsonify(event_jsons)
+
